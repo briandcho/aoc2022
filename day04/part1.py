4c1d9cb98a368e9972cbe4893e1dcc1e48a365d2
Brian Cho <briandcho@yahoo.com>
2022-12-03 22:56:32 -0800

    day04

---
+++ day04/part1.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    lines = (line for line in s.split("\n") if line)
+    fully_contained = 0
+    for section1, section2 in [line.split(",") for line in lines]:
+        x, y = section1.split("-")
+        s1 = set(range(int(x), int(y) + 1))
+        x, y = section2.split("-")
+        s2 = set(range(int(x), int(y) + 1))
+        if s1.issubset(s2) or s1.issuperset(s2):
+            fully_contained += 1
+    return fully_contained
+
+
+INPUT_S = """\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"""
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
