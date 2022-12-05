8c3d2fe2f3fb22a48920e6df99f3134acc5074c4
Brian Cho <briandcho@yahoo.com>
2022-12-04 21:36:06 -0800

    day05

---
+++ day05/part2.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    # section top
+    lines = (line for line in s.split("\n"))
+    rows = []
+    n_stacks = 0
+    for line in lines:
+        if not line:
+            break
+        row = []
+        while line:
+            box = line[:4].strip()
+            row.append(box)
+            line = line[4:]
+        if not row[0].isnumeric():
+            rows.append(row)
+        else:
+            n_stacks = len(row)
+    stacks = [[] for _ in range(n_stacks)]
+    for row in reversed(rows):
+        for i, val in enumerate(row):
+            if val:
+                stacks[i].append(val)
+
+    # section instructions
+    for line in lines:
+        if not line:
+            break
+        _, qty, _, src, _, dst = line.split()
+        qty = int(qty)
+        src = int(src) - 1
+        dst = int(dst) - 1
+        boxes = stacks[src][-qty:]
+        stacks[dst] += boxes
+        stacks[src] = stacks[src][:-qty]
+    return "".join([st[-1][1] for st in stacks])
+
+
+INPUT_S = """\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+EXPECTED = "MCD"
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
